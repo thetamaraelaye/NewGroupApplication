950b173ba4988b4b717001730ed80a3b63250f07
Tamara Elaye <tamaraelaye@gmail.com>
2023-09-29 16:01:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/newgroupapplication/views/auth/SplashScreenFragment.kt
@@ -0,0 +1,22 @@
+package com.example.newgroupapplication.views.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class SplashScreenFragment : Fragment() {
+
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//
+//    }
+
+
+}

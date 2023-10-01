b655ade53bb4092151e16987884116a6e4816c3f
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-01 01:56:12 +0100

    "feat(signup): add binding and nav onClick to the buttons"

--- app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpFragment.kt
+++ app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpFragment.kt
@@ -5,15 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.newgroupapplication.R
+import com.example.newgroupapplication.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        goToLogin()
+        goToSignup()
+    }
+
+    private fun goToLogin(){
+        //navigate to login fragment
+        binding.login.setOnClickListener{
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+    }
+    private fun goToSignup(){
+        //navigate to signup screen
+        binding.openaccountbutton.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_signUpPersonalFragment)
+        }
     }
 
 }

d60d05e2929ca19372f9931f8a180b883ac2252f
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-01 20:26:30 +0100

    "feat(resetpw): start page one of reset password"

---
+++ app/src/main/java/com/example/newgroupapplication/views/auth/resetpassword/PhoneNumberFragment.kt
@@ -0,0 +1,32 @@
+package com.example.newgroupapplication.views.auth.resetpassword
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.newgroupapplication.databinding.FragmentPhoneNumberBinding
+
+
+class PhoneNumberFragment : Fragment() {
+
+    private lateinit var binding: FragmentPhoneNumberBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPhoneNumberBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        doResetStepOne()
+    }
+
+    private fun doResetStepOne(){
+
+    }
+
+
+}

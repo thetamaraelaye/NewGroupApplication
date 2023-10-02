743b5cd4770e2fcb55908d4f443999df5c559174
brycen <olakunlebrycen@gmail.com>
2023-10-02 11:22:03 +0100

    feat (signupaddress): finish without fonts and blue asterix ' * ' coloring also add an image

--- app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpAddressFragment.kt
+++ app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpAddressFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.newgroupapplication.R
+import androidx.navigation.fragment.findNavController
+import com.example.newgroupapplication.databinding.FragmentSignUpAddressBinding
 
 
 class SignUpAddressFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpAddressBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_address, container, false)
+        binding = FragmentSignUpAddressBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        doAddress()
+    }
+
+    private fun doAddress(){
+        binding.AddressBackbtn.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+    }
 
 }

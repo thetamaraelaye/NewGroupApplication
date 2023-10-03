75d5870a696d1136b2a2bc8ed34708dc49123d1d
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-03 10:44:13 +0100

    "feat(signup_bank): add functionality and binding to the screen"

--- app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpBankFragment.kt
+++ app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpBankFragment.kt
@@ -5,18 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.newgroupapplication.R
+import androidx.navigation.fragment.findNavController
+import com.example.newgroupapplication.databinding.FragmentSignUpBankBinding
 
 
 class SignUpBankFragment : Fragment() {
 
-
+    private lateinit var binding : FragmentSignUpBankBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_bank, container, false)
+        binding = FragmentSignUpBankBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        doSignUpBank()
+    }
+
+    private fun doSignUpBank (){
+        binding.imageView6.setOnClickListener{
+            findNavController().popBackStack()
+        }
+
+
+
+
     }
 
 }

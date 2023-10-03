e988e73f4878556100183b5b5a4c5c6ef2eec051
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-03 10:42:29 +0100

    "feat(signup_personal): add functionality and binding to the screen"

--- app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpPersonalFragment.kt
+++ app/src/main/java/com/example/newgroupapplication/views/auth/SignUp/SignUpPersonalFragment.kt
@@ -5,15 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.newgroupapplication.R
+import com.example.newgroupapplication.databinding.FragmentSignUpPersonalBinding
 
 
 class SignUpPersonalFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignUpPersonalBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_personal, container, false)
+        binding = FragmentSignUpPersonalBinding.inflate(layoutInflater)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        doSignUpPersonal()
+    }
+
+    private fun doSignUpPersonal(){
+        //controls navigation to previous page
+        binding.imageView5.setOnClickListener{
+            findNavController().popBackStack()
+        }
+
+        //navigate to signup address screen
+        binding.button2.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpPersonalFragment_to_signUpAddressFragment)
+        }
+
+
+    }
+
 
 }

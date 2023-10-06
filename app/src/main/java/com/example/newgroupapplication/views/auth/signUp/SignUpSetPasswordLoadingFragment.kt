466e96511189c4e6ad81dbf0de6d0e65053f88e4
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-06 08:24:29 +0100

    "feat(signup_verification): add binding and auto-navigate after 2seconds to fragment"

---
+++ app/src/main/java/com/example/newgroupapplication/views/auth/signUp/SignUpSetPasswordLoadingFragment.kt
@@ -0,0 +1,36 @@
+package com.example.newgroupapplication.views.auth.SignUp
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.newgroupapplication.R
+import com.example.newgroupapplication.databinding.FragmentSignUpSetPasswordLoadingBinding
+
+class SignUpSetPasswordLoadingFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpSetPasswordLoadingBinding
+    var handler: Handler? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignUpSetPasswordLoadingBinding.inflate(layoutInflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleSetPasswordLoading()
+    }
+
+    private fun handleSetPasswordLoading() {
+        handler = Handler()
+        handler!!.postDelayed({
+            findNavController().navigate(R.id.action_signUpSetPasswordLoadingFragment_to_accountSuccessFragment)
+        }, 2000)
+    }
+
+}

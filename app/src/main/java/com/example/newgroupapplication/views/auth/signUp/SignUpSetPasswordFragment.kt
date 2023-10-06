e3a04d368d1907f278f993225deeaf9df47d2e5d
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-06 08:20:29 +0100

    "feat(signup_personal): add binding and navigation to the fragment"

--- app/src/main/java/com/example/newgroupapplication/views/auth/signUp/SignUpSetPasswordFragment.kt
+++ app/src/main/java/com/example/newgroupapplication/views/auth/signUp/SignUpSetPasswordFragment.kt
@@ -5,16 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.newgroupapplication.R
+import com.example.newgroupapplication.databinding.FragmentSignUpSetPasswordBinding
 
 class SignUpSetPasswordFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignUpSetPasswordBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_set_password, container, false)
+       binding = FragmentSignUpSetPasswordBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        handleSetPassword()
+    }
+
+    private fun handleSetPassword() {
+        //navigate to prev screen
+        binding.imageView18.setOnClickListener{
+            findNavController().popBackStack()
+        }
+
+        //navigate to password loading fragment
+        binding.button6.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpSetPasswordFragment_to_signUpSetPasswordLoadingFragment)
+        }
     }
 
 }

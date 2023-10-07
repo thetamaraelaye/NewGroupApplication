654330f3a8595e47599a266a081d4d333dfa75c8
Tamara Elaye <tamaraelaye@gmail.com>
2023-10-07 22:27:35 +0100

    "feat(account_success): bolden the last text of the account create message"

--- app/src/main/java/com/example/newgroupapplication/views/auth/signUp/AccountSuccessFragment.kt
+++ app/src/main/java/com/example/newgroupapplication/views/auth/signUp/AccountSuccessFragment.kt
@@ -1,6 +1,10 @@
 package com.example.newgroupapplication.views.auth.signUp
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,7 +28,27 @@ class AccountSuccessFragment : Fragment() {
     }
 
     private fun handleAccountSuccess() {
+        val text = "You have created an account successfully. Your account number is 0654321891"
 
+        // Split the text into words
+        val words = text.split(" ")
+        val lastWord = words.last() // Get the last word
+
+        // Create a SpannableString
+        val spannableString = SpannableString(text)
+
+        // Create a StyleSpan to make the last word bold
+        val boldSpan = StyleSpan(Typeface.BOLD)
+
+        // Find the start and end positions of the last word
+        val startIndex = text.lastIndexOf(lastWord)
+        val endIndex = startIndex + lastWord.length
+
+        // Apply the StyleSpan to make the last word bold
+        spannableString.setSpan(boldSpan, startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // Set the SpannableString as the text of the TextView
+        binding.textView19.text = spannableString
     }
 
 }
